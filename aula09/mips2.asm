0d82dc366e4b3bf8ed18ae57a09fc1916f45da7f
unknown <dcapitao@ua.pt>
2021-12-14 19:41:50 +0000

    update

---
+++ aula09/mips2.asm
@@ -0,0 +1,14 @@
+# double f2c(double ft)
+# { return (5.0 / 9.0 * (ft – 32.0)); }
+
+#	ft = $f12
+
+	.data
+const1:	.double 5.0
+const2: .double 9.0
+const3: .double 32.0
+	.text
+	.globl main
+	
+main:	
+
